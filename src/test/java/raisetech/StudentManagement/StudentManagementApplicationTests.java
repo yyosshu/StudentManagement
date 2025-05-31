fa3e260f9f125df6f396a9d97bea11b28aca7a43
yoshi <yyosshu@gmail.com>
2025-05-31 22:51:20 +0900

    初回コミット

---
+++ src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
