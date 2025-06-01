e69febb8f5a8a6c8db6449c4bab18100dc5fbfaf
yyosshu <yyosshu@gmail.com>
2025-06-01 10:10:27 +0900

    DBを使わないGET/POSTマッピングメソッドを追加。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,21 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
+
+  private String name = "Java Test";
+  private String age = "30";
+  private final Map<String, String> map = new HashMap<>();
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
@@ -16,5 +24,40 @@ public class StudentManagementApplication {
   @GetMapping("/hello")
   public String hello() {
     return "Hello, World!";
+  }
+
+  @GetMapping("/name")
+  public String getName() {
+    return name;
+  }
+
+  @PostMapping("/name")
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @GetMapping("/info")
+  public String getInfo() {
+    return name + " " + age;
+  }
+
+  @PostMapping("/info")
+  public void setInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @GetMapping("/infoMap")
+  public String getInfoMap() {
+    return map.keySet().stream()
+        .map(name -> name + " " + map.get(name))
+        .collect(Collectors.joining(","));
+  }
+
+  @PostMapping("/infoMap")
+  public void setInfoMap(String name, String age) {
+    if (!map.containsKey(name)) {
+      map.put(name, age);
+    }
   }
 }
